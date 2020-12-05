d6e3e7974dab71bb09105e0ed7db66cf8220a3e5
zankhana rana <zankhanarana9@gmail.com>
2020-12-04 20:31:22 -0600

    Added account reguster and profile templates

--- src/main/java/com/zrana/springit/SpringitApplication.java
+++ src/main/java/com/zrana/springit/SpringitApplication.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 public class SpringitApplication {
@@ -25,18 +26,9 @@ public class SpringitApplication {
 	PrettyTime prettyTime() {
 		return new PrettyTime();
 	}
-	//@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository) {
-		return args -> {
-			Link link = new Link("Getting started with spring boot 2",
-					"https://therealdenvega.com/spring-boot-2");
-			linkRepository.save(link);
 
-			Comment comment = new Comment("This spring boot 2 link is awesome!", link);
-			commentRepository.save(comment);
-
-			link.addComment(comment);
-			System.out.println("We just inserted link and a comment");
-		};
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
 	}
 }
